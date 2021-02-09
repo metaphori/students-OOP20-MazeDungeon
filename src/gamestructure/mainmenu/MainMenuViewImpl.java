47a0292f4d8deaebf48aaa38fc2bc4511696cc16
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-02-09 11:45:54 +0100

    Add bullet directory, modify MainMenuViewImpl

--- src/gamestructure/mainmenu/MainMenuViewImpl.java
+++ src/gamestructure/mainmenu/MainMenuViewImpl.java
@@ -1,6 +1,7 @@
 package gamestructure.mainmenu;
 
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -10,24 +11,30 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainMenuViewImpl implements MainMenuView {
 
     private final JFrame frame = new JFrame();
+    private static double WIDTH_RATIO = 0.67; 
+    private static double HEIGHT_RATIO = 0.736;
 
     public MainMenuViewImpl() {
         BufferedImage img;
         JLabel label;
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setSize(750, 750);
+        this.frame.setResizable(false);
+        this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), 
+                (int) (screen.getHeight() * HEIGHT_RATIO)));
+        final JPanel mainPanel = new JPanel(new GridBagLayout());
+        this.frame.setContentPane(mainPanel);
 
         try {
-            img = ImageIO.read(new File("res//images//MainMenù//MainMenù1.png"));
+            img = ImageIO.read(new File("resources//images//MainMenu//MainMenu3.png"));
             label = new JLabel(new ImageIcon(img));
             this.frame.add(label);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -36,6 +43,7 @@ public class MainMenuViewImpl implements MainMenuView {
      * @Override
      */
     public void show() {
+        this.frame.pack();
         this.frame.setVisible(true);
     }
 
