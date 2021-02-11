9c54735c70ce2e970c892fffbe66b018539d0486
Malucelli Nicolo <nicolo.malucelli@studio.unibo.it>
2021-02-11 11:21:40 +0100

    image movement on jframe

--- src/gamestructure/game/GameViewImpl.java
+++ src/gamestructure/game/GameViewImpl.java
@@ -1,18 +1,17 @@
 package gamestructure.game;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GameViewImpl implements GameView {
 
@@ -21,30 +20,28 @@ public class GameViewImpl implements GameView {
     private static double WIDTH_RATIO = 0.67; 
     private static double HEIGHT_RATIO = 0.74; 
     private static double ASPECT_RATIO = 1.6;
+    private Timer timer;
 
     public GameViewImpl() {
+        
         this.frame = new JFrame();
         this.frame.setResizable(false);
         this.frame.setTitle("MazeDungeon");
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        final JPanel backgroundPanel = new JPanel(new GridBagLayout());
-        this.frame.setContentPane(backgroundPanel);
-        try {
-            final int height = (int) (screen.getHeight() * HEIGHT_RATIO);
-            final int width = (int) (screen.getWidth() * WIDTH_RATIO);
-            //final int width = (int) (height * ASPECT_RATIO);
-            final BufferedImage room = ImageIO.read(new File("resources//images//Room//room.png"));
-            final JLabel picLabel = new JLabel(new ImageIcon(room.getScaledInstance(width, height, Image.SCALE_SMOOTH)));
-            backgroundPanel.add(picLabel);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), (int) (screen.getHeight() * HEIGHT_RATIO)));
+
+        final GamePanel gamePanel = new GamePanel();
+        //gamePanel.setLayout(new GridBagLayout()); TODO
+        this.frame.add(gamePanel);
+        timer = new Timer(25, gamePanel);
+        timer.start();
 
     }
 
     @Override
     public void show() {
-        this.frame.pack();
+        //this.frame.pack();
         this.frame.setLocation(screen.width / 2 - this.frame.getSize().width / 2, 
                                screen.height / 2 - this.frame.getSize().height / 2);
         this.frame.setVisible(true);
@@ -55,4 +52,40 @@ public class GameViewImpl implements GameView {
         this.frame.setVisible(false);
     }
 
+    private class GamePanel extends JPanel implements ActionListener{
+        private static final long serialVersionUID = 1L;
+        private Image coin1;
+        private Image coin2;
+        private Image room;
+        public int coinX1 = 100;
+        public int coinX2 = 1000;
+
+        GamePanel() {
+            ImageIcon ii = new ImageIcon("resources//images//Objects//Coin//coin.png");
+            coin1 = ii.getImage();
+            coin2 = ii.getImage();
+            ii = new ImageIcon("resources//images//Room//room.png");
+            room = ii.getImage();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(room, 0, 0, null);
+            g.drawImage(this.coin1, coinX1, 100, null);
+            g.drawImage(this.coin2, coinX2, 0, null);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            coinX1+=1;
+            coinX2-=1;
+
+            repaint();
+        }
+
+    }
+
 }
+
+
