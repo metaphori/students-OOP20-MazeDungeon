126a3eb7df4bb292805a9763532a162483dc0edf
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-02-13 12:40:54 +0100

    Add some usefull istruction for configure JButton

--- src/gamestructure/mainmenu/MainMenuViewImpl.java
+++ src/gamestructure/mainmenu/MainMenuViewImpl.java
@@ -1,17 +1,18 @@
 package gamestructure.mainmenu;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class MainMenuViewImpl implements MainMenuView {
 
@@ -21,22 +22,24 @@ public class MainMenuViewImpl implements MainMenuView {
     private static double HEIGHT_RATIO = 0.736;
 
     public MainMenuViewImpl() {
-        BufferedImage img;
-        JLabel label;
+        final JLabel label = new JLabel(new ImageIcon("resources//images//MainMenu//MainMenu3.png"));
+        final JButton newGame = new JButton("NEW GAME");
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setResizable(false);
         this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), 
                 (int) (screen.getHeight() * HEIGHT_RATIO)));
         final JPanel mainPanel = new JPanel(new GridBagLayout());
+        newGame.setLayout(null);
+        newGame.setBounds(160, 350, 300, 30);
+        newGame.setFont(new Font("MS Gothic", Font.BOLD, 30));
+        newGame.setBackground(Color.DARK_GRAY); //Colore del background del bottone
+        newGame.setForeground(new Color(23, 11, 0)); //Colore del testo
+        newGame.setBorder(new LineBorder(Color.BLACK)); //Colore del bordo del bottone
+        newGame.setFocusPainted(false); //Disabilita il paint del focus sul testo del bottone
+        label.setLayout(null);
+        label.add(newGame);
+        mainPanel.add(label);
         this.frame.setContentPane(mainPanel);
-
-        try {
-            img = ImageIO.read(new File("resources//images//MainMenu//MainMenu3.png"));
-            label = new JLabel(new ImageIcon(img));
-            this.frame.add(label);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
