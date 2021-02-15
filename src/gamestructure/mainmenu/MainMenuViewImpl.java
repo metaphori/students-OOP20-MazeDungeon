e2dc61c6ecc6046ff31c78a50cebe7ce31998d58
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-02-15 09:41:30 +0100

    Modified mainmenuview

--- src/gamestructure/mainmenu/MainMenuViewImpl.java
+++ src/gamestructure/mainmenu/MainMenuViewImpl.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
+
 public class MainMenuViewImpl implements MainMenuView {
 
     private final JFrame frame = new JFrame();
@@ -22,23 +23,23 @@ public class MainMenuViewImpl implements MainMenuView {
     private static double HEIGHT_RATIO = 0.736;
 
     public MainMenuViewImpl() {
-        final JLabel label = new JLabel(new ImageIcon("resources//images//MainMenu//MainMenu3.png"));
-        final JButton newGame = new JButton("NEW GAME");
+        final JLabel lblBackground = new JLabel(new ImageIcon("resources//images//MainMenu//MainMenu3.png"));
+        final JButton btnNewGame = new JButton("NEW GAME"); //MΛZΣ DUNGΣӨП
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setResizable(false);
         this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), 
                 (int) (screen.getHeight() * HEIGHT_RATIO)));
         final JPanel mainPanel = new JPanel(new GridBagLayout());
-        newGame.setLayout(null);
-        newGame.setBounds(160, 350, 300, 30);
-        newGame.setFont(new Font("MS Gothic", Font.BOLD, 30));
-        newGame.setBackground(Color.DARK_GRAY); //Colore del background del bottone
-        newGame.setForeground(new Color(23, 11, 0)); //Colore del testo
-        newGame.setBorder(new LineBorder(Color.BLACK)); //Colore del bordo del bottone
-        newGame.setFocusPainted(false); //Disabilita il paint del focus sul testo del bottone
-        label.setLayout(null);
-        label.add(newGame);
-        mainPanel.add(label);
+        btnNewGame.setBounds(160, 350, 300, 30);
+        btnNewGame.setFont(new Font("MS Gothic", Font.BOLD, 30));
+        btnNewGame.setBackground(Color.DARK_GRAY); //Colore del background del bottone
+        btnNewGame.setForeground(new Color(23, 11, 0)); //Colore del testo
+        btnNewGame.setBorder(new LineBorder(Color.BLACK)); //Colore del bordo del bottone
+        btnNewGame.setFocusPainted(false); //Disabilita il paint del focus sul testo del bottone
+        btnNewGame.setLayout(null);
+        lblBackground.setLayout(null);
+        lblBackground.add(btnNewGame);
+        mainPanel.add(lblBackground);
         this.frame.setContentPane(mainPanel);
     }
 
