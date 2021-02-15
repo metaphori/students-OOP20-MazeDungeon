325d30ce15f92630e5079d493ff089cc8693a8a6
Malucelli Nicolo <nicolo.malucelli@studio.unibo.it>
2021-02-15 09:51:38 +0100

    add resize methods (actualy unabled

--- src/gamestructure/game/GameViewImpl.java
+++ src/gamestructure/game/GameViewImpl.java
@@ -1,6 +1,5 @@
 package gamestructure.game;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
@@ -13,6 +12,7 @@ import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -24,7 +24,8 @@ public class GameViewImpl implements GameView {
     private GameController controller;
     private final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     private final JFrame frame;
-    private final Color frameColor;
+    private static int NATIVE_WIDTH = 1920;
+    private static int NATIVE_HEIGHT = 1080;
     private static double WIDTH_RATIO = 0.67; 
     private static double HEIGHT_RATIO = 0.74; 
     private static double ASPECT_RATIO = 1.6;
@@ -37,11 +38,12 @@ public class GameViewImpl implements GameView {
         this.frame.setResizable(false);
         this.frame.setTitle("MazeDungeon");
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), (int) (screen.getHeight() * HEIGHT_RATIO)));
-        this.frameColor = Color.BLUE;
-        this.frame.setBackground(this.frameColor);
+        //this.frame.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), (int) (screen.getHeight() * HEIGHT_RATIO)));
+        this.frame.setSize(new Dimension((int) (NATIVE_WIDTH * WIDTH_RATIO), (int) (NATIVE_HEIGHT * HEIGHT_RATIO)));
+        //final JLabel lblBackground = new JLabel(new ImageIcon("resources//images//Room//room.png"));
         final GamePanel gamePanel = new GamePanel();
-        gamePanel.setSize(new Dimension((int) (screen.getWidth() * WIDTH_RATIO), (int) (screen.getHeight() * HEIGHT_RATIO)));
+        //gamePanel.add(lblBackground);
+        gamePanel.setSize(this.frame.getSize());
         //gamePanel.setLayout(new GridBagLayout()); //TODO
         this.frame.add(gamePanel);
         timer = new Timer(PERIOD, gamePanel);
@@ -61,45 +63,29 @@ public class GameViewImpl implements GameView {
         this.frame.setVisible(false);
     }
 
-    private class GamePanel extends JPanel implements ActionListener{
+    private class GamePanel extends JPanel implements ActionListener {
         private static final long serialVersionUID = 1L;
-        private Image room;
+        private final Image room;
         private Point2D position = new Point2D(800, 300);
-        private Vector2D vector = new Vector2D(2, 2);
+        private final Vector2D vector = new Vector2D(0, 0);
         private final List<Image> images = new LinkedList<>();
         private int index = 0;
 
         GamePanel() {
-            ImageIcon ii = new ImageIcon("resources//images//Objects//Coin//coin1.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin2.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin3.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin4.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin5.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin6.png");
-            images.add(ii.getImage());
-            ii = new ImageIcon("resources//images//Objects//Coin//coin7.png");
-            images.add(ii.getImage());
-
-            //System.out.println(ii.getIconWidth());
-
-            ii = new ImageIcon("resources//images//Room//room.png");
-            room = ii.getImage();
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin1.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin2.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin3.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin4.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin5.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin6.png")));
+            images.add(adaptImage(new ImageIcon("resources//images//Objects//Coin//coin7.png")));
+            room = adaptImage(new ImageIcon("resources//images//Room//room.png"));
         }
 
         @Override
         protected void paintComponent(final Graphics g) {
             super.paintComponent(g);
             g.drawImage(room, 0, 0, null);
-            /*
-            g.drawImage(this.coin1, coinX1, 100, null);
-            g.drawImage(this.coin2, coinX2, 0, null);
-            */
-
             g.drawImage(images.get(index), position.getX(), position.getY(), null); 
             Toolkit.getDefaultToolkit().sync();
             position = position.sum(vector);
@@ -123,6 +109,20 @@ public class GameViewImpl implements GameView {
     @Override
     public void render() {
         // TODO Auto-generated method stub
+    }
+
+    private int getNewWidth(int width) {
+        //return width * (int) screen.getWidth() / NATIVE_WIDTH;
+        return width;
+    }
+
+    private int getNewHeight(int height) {
+        //return height * (int) screen.getHeight() / NATIVE_HEIGHT;
+        return height;
+    }
+
+    private Image adaptImage(ImageIcon img) {
+        return img.getImage().getScaledInstance(getNewWidth(img.getIconWidth()), getNewHeight(img.getIconHeight()), Image.SCALE_SMOOTH);
     }
 
 }
