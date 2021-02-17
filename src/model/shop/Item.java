a31c6b5ab4a4913d6fafc1696a12eaf15ce198b3
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2021-02-17 12:21:03 +0100

    Start create Item

--- src/model/shop/Item.java
+++ src/model/shop/Item.java
@@ -1,5 +1,63 @@
 package model.shop;
 
-public interface Item {
+import java.util.Optional;
+
+public final class Item {
+
+    private String name;
+
+    private int cost;
+
+    private int demage;
+
+    private int speed;
+
+    private int speedAttack;
+
+    private  Optional<Integer> health;
+
+    private Item(final String name, final int cost, final int demage, final int speed, final int speedAttack, final Optional<Integer> health) {
+        super();
+        this.name = name;
+        this.cost = cost;
+        this.demage = demage;
+        this.speed = speed;
+        this.speedAttack = speedAttack;
+        this.health = health;
+    }
+
+    public class Builder {
+
+        private final String name;
+        private final int cost;
+        private final int demage;
+        private final int speed;
+        private final int speedAttack;
+        private  Optional<Integer> health = Optional.empty();
+
+        public Builder(final String name, final int cost, final int demage, final int speed, final int speedAttack) {
+            this.name = name;
+            this.cost = cost;
+            this.demage = demage;
+            this.speed = speed;
+            this.speedAttack = speedAttack;
+        }
+
+        /**
+         * @param health
+         * @return this
+         */
+        public Builder addHelath(final int health) {
+            this.health = Optional.of(health);
+            return this;
+        }
+
+        /**
+         * @return Item
+         */
+        public Item build() {
+            return new Item(this.name, this.cost, this.demage, this.speed, this.speedAttack, this.health);
+        }
+    }
 
 }
---
+++ src/model/shop/ShopImpl.java
@@ -0,0 +1,10 @@
+package model.shop;
+
+public class ShopImpl {
+    public Item getArthemideBow() {
+        
+        Item i = new Item.Builder("ArthemideBow", 2, 1, 0, 0).build();
+        return null;
+        //return new Item.Builder("ArthemideBow", 2, 1, 0, 0).build();
+    }
+}
