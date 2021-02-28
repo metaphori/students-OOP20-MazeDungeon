37806676b5b14cbd04f2f114bfd0ad2882a415ce
Davide Di Marco <davide.dimarco5@studio.unibo.it>
2021-02-28 09:14:42 +0100

    correct shop button in linux

--- src/gamestructure/game/GameViewImpl.java
+++ src/gamestructure/game/GameViewImpl.java
@@ -183,7 +183,7 @@ public class GameViewImpl implements GameView, KeyListener {
         final int heigth = img.getIconHeight();
         return img.getImage().getScaledInstance(width, heigth, Image.SCALE_SMOOTH);
     }
-    
+
     /**
      * 
      */
