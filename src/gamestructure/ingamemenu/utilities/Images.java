add234a93f02d5c138d04ab174eeec1a2e2da361
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2021-03-08 17:42:25 +0100

    Remove some warnings ad add Javadoc

--- src/gamestructure/ingamemenu/utilities/Images.java
+++ src/gamestructure/ingamemenu/utilities/Images.java
@@ -1,5 +1,48 @@
 package gamestructure.ingamemenu.utilities;
 
 public enum Images {
-    BACKGROUNDMENU, BACKGROUNDSHOP, BTNRESUME, BTNEXIT, BTNSHOP, BTNRETURNMENU, BTNARTHEMIDEBOW, BTNHEALTH, BTNHERMESBOOTS, BTNZEUSBOLT, BTNORACLEAMULET
+    /**
+     * the background menu image.
+     */
+    BACKGROUNDMENU,
+    /**
+     * the background shop image.
+     */
+    BACKGROUNDSHOP,
+    /**
+     * the button resume image.
+     */
+    BTNRESUME,
+    /**
+     * the button exit image.
+     */
+    BTNEXIT, 
+    /**
+     * the button shop image.
+     */
+    BTNSHOP,
+    /**
+     * the button return to menu image.
+     */
+    BTNRETURNMENU,
+    /**
+     * the button Arthemide Bow Item image.
+     */
+    BTNARTHEMIDEBOW, 
+    /**
+     * the button Health Item image.
+     */
+    BTNHEALTH,
+    /**
+     * the button HermesBoots Item image.
+     */
+    BTNHERMESBOOTS,
+    /**
+     * the button ZeusBolt Item image.
+     */
+    BTNZEUSBOLT,
+    /**
+     * the button OracleAmulet Item image.
+     */
+    BTNORACLEAMULET
 }
