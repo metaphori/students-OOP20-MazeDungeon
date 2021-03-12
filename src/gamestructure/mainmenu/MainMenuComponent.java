0783e207be5706d3179129c70d3f5cc189a91590
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-03-12 15:23:01 +0100

    Refactor MainMenu using stream and lambda

---
+++ src/gamestructure/mainmenu/MainMenuComponent.java
@@ -0,0 +1,9 @@
+package gamestructure.mainmenu;
+
+public enum MainMenuComponent {
+    BTN_NEW_GAME,
+    BTN_CREDITS,
+    BTN_EXIT,
+    LBL_CREDITS,
+    LBL_BACKGROUND;
+}
---
+++ src/gamestructure/mainmenu/Pair.java
@@ -0,0 +1,25 @@
+package gamestructure.mainmenu;
+
+public class Pair<X, Y> {
+    private final X x;
+    private final Y y;
+
+    public Pair(final X x, final Y y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return the value of the x.
+     */
+    public X getX() {
+        return x;
+    }
+
+    /**
+     * @return the value of the y.
+     */
+    public Y getY() {
+        return y;
+    }
+}
