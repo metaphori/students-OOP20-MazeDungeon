22832f4e1a6b40859b64936149960dda3c3cc45e
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-03-13 10:14:20 +0100

    Refactor MainMenuComponent

--- src/gamestructure/mainmenu/MainMenuComponent.java
+++ src/gamestructure/mainmenu/MainMenuComponent.java
@@ -1,28 +1,47 @@
 package gamestructure.mainmenu;
 
+import javax.swing.ImageIcon;
+
+import gamestructure.PathGetter;
+
 public enum MainMenuComponent {
     /**
      * The button that start a new game.
      */
-    BTN_NEW_GAME,
+    BTN_NEW_GAME(new ImageIcon(PathResources.IMAGE_PATH + "NewGame.png")),
 
     /**
      * The button that show the credits.
      */
-    BTN_CREDITS,
+    BTN_CREDITS(new ImageIcon(PathResources.IMAGE_PATH + "CreditsButton.png")),
 
     /**
      * The button for exit the game.
      */
-    BTN_EXIT,
+    BTN_EXIT(new ImageIcon(PathResources.IMAGE_PATH + "Exit.png")),
 
     /**
      * The label where the credits are showed.
      */
-    LBL_CREDITS,
+    LBL_CREDITS(new ImageIcon(PathResources.IMAGE_PATH + "lblCredits.png")),
 
     /**
      * The label that contains the background image.
      */
-    LBL_BACKGROUND;
+    LBL_BACKGROUND(new ImageIcon(PathResources.IMAGE_PATH + "MainMenuBackground.png"));
+
+    private final ImageIcon img;
+
+    MainMenuComponent(final ImageIcon img) {
+        this.img = img;
+    }
+
+    private static class PathResources {
+        private static final PathGetter PATH_GETTER = new PathGetter();
+        private static final String IMAGE_PATH = PATH_GETTER.getPortablePath("resources/images/MainMenu/");
+    }
+
+    public ImageIcon getImage() {
+        return this.img;
+    }
 }
--- src/gamestructure/mainmenu/MainMenuViewImpl.java
+++ src/gamestructure/mainmenu/MainMenuViewImpl.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -15,7 +14,6 @@ import javax.swing.JLayeredPane;
 import javax.swing.border.LineBorder;
 
 import gamestructure.Pair;
-import gamestructure.PathGetter;
 import gamestructure.WindowUtilities;
 import model.common.ResizableRectangle;
 
@@ -25,7 +23,6 @@ public class MainMenuViewImpl implements MainMenuView {
     private static final int MENU_HEIGHT = 727;
     private static final Color BACKGROUND_COLOR = new Color(11, 19, 30);
     private final WindowUtilities windowUtilities = new WindowUtilities();
-    private final PathGetter pathGetter = new PathGetter();
     private final JFrame frame = new JFrame();
 
     private final MainMenuController controller;
@@ -62,7 +59,6 @@ public class MainMenuViewImpl implements MainMenuView {
     private class MenuPanel extends JLayeredPane {
         private static final long serialVersionUID = 6848873930359078496L;
 
-        private final String imagesPath = pathGetter.getPortablePath("resources/images/MainMenu/");
         private final ResizableRectangle newGameButtonPosition = new ResizableRectangle(160, 340, 300, 45);
         private final ResizableRectangle creditsButtonPosition = new ResizableRectangle(160, 430, 300, 45);
         private final ResizableRectangle exitButtonPosition = new ResizableRectangle(160, 520, 300, 45);
@@ -76,22 +72,32 @@ public class MainMenuViewImpl implements MainMenuView {
             this.initializeComponentsMap();
             this.componentsMap.entrySet().stream().forEach(e -> {
                 e.getValue().getY().mul(windowUtilities.getScreenRatio());
+                if (e.getValue().getX() instanceof JLabel) {
+                    final JLabel lbl = (JLabel) e.getValue().getX();
+                    lbl.setIcon(windowUtilities.resizeImage(e.getKey().getImage(), e.getValue().getY()));
+                }
                 this.configureComponents(e);
                 e.getValue().getX().setBounds(e.getValue().getY());
                 this.add(e.getValue().getX(), e.getKey().equals(MainMenuComponent.LBL_BACKGROUND) ? JLayeredPane.DEFAULT_LAYER : JLayeredPane.PALETTE_LAYER);
             });
 
-            
             this.componentsMap.entrySet().stream().filter(e -> e.getValue().getX() instanceof JButton).forEach(e -> {
+                final JButton btn = (JButton) e.getValue().getX();
+                btn.setIcon(windowUtilities.resizeImage(e.getKey().getImage(), e.getValue().getY()));
                 this.configureButtonGraphics((JButton) e.getValue().getX());
             });
         }
 
         private void configureComponents(final Entry<MainMenuComponent, Pair<JComponent, ResizableRectangle>> componentEntry) {
             switch (componentEntry.getKey()) {
+            case BTN_NEW_GAME:
+                final JButton btnNewGame = (JButton) (componentEntry.getValue().getX());
+                btnNewGame.addActionListener(e -> {
+                    controller.newGame();
+                });
+                break;
             case BTN_CREDITS:
                 final JButton btnCredits = (JButton) (componentEntry.getValue().getX());
-                btnCredits.setIcon(windowUtilities.resizeImage(new ImageIcon(this.imagesPath + "CreditsButton.png"), componentEntry.getValue().getY()));
                 btnCredits.addActionListener(e -> {
                     this.lblCreditsIsVisible = !this.lblCreditsIsVisible;
                     this.componentsMap.entrySet().stream()
@@ -102,26 +108,11 @@ public class MainMenuViewImpl implements MainMenuView {
                 break;
             case BTN_EXIT:
                 final JButton btnExit = (JButton) (componentEntry.getValue().getX());
-                btnExit.setIcon(windowUtilities.resizeImage(new ImageIcon(this.imagesPath + "Exit.png"), componentEntry.getValue().getY()));
                 btnExit.addActionListener(e -> {
                     MainMenuViewImpl.this.hide();
                 });
                 break;
-            case BTN_NEW_GAME:
-                final JButton btnNewGame = (JButton) (componentEntry.getValue().getX());
-                btnNewGame.setIcon(windowUtilities.resizeImage(new ImageIcon(this.imagesPath + "NewGame.png"), componentEntry.getValue().getY()));
-                btnNewGame.addActionListener(e -> {
-                    controller.newGame();
-                });
-                break;
-            case LBL_BACKGROUND:
-                final JLabel lblBackGround = (JLabel) (componentEntry.getValue().getX());
-                lblBackGround.setIcon(windowUtilities.resizeImage(new ImageIcon(this.imagesPath + "MainMenuBackground.png"), componentEntry.getValue().getY()));
-                break;
             default:
-                final JLabel lblCredits = (JLabel) (componentEntry.getValue().getX());
-                lblCredits.setVisible(false);
-                lblCredits.setIcon(windowUtilities.resizeImage(new ImageIcon(this.imagesPath + "lblCredits.png"), componentEntry.getValue().getY()));
                 break;
             }
         }
@@ -132,6 +123,10 @@ public class MainMenuViewImpl implements MainMenuView {
             this.componentsMap.put(MainMenuComponent.BTN_EXIT, new Pair<>(new JButton(), exitButtonPosition));
             this.componentsMap.put(MainMenuComponent.LBL_BACKGROUND, new Pair<>(new JLabel(), lblBackgroundPosition));
             this.componentsMap.put(MainMenuComponent.LBL_CREDITS, new Pair<>(new JLabel(), lblCreditsPosition));
+            this.componentsMap.entrySet().stream()
+                                         .filter(e -> e.getKey().equals(MainMenuComponent.LBL_CREDITS))
+                                         .map(e -> e.getValue().getX())
+                                         .findAny().get().setVisible(false);
         }
 
         private void configureButtonGraphics(final JButton btn) {
