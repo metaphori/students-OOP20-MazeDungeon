e038a2de29487c5d39894ce29334676ae3ff0989
Davide Di Marco <davide.dimarco5@studio.unibo.it>
2021-03-13 11:57:02 +0100

    remove warning

--- src/model/common/VectorDirection.java
+++ src/model/common/VectorDirection.java
@@ -1,14 +1,26 @@
 package model.common;
 
 public enum VectorDirection {
+    /**
+     * Direction up for movement.
+     */
     UP(new Vector2D(0, -1)),
+    /**
+     * Direction down for movement.
+     */
     DOWN(new Vector2D(0, 1)),
+    /**
+     * Direction left for movement.
+     */
     LEFT(new Vector2D(-1, 0)),
+    /**
+     * Direction right for movement.
+     */
     RIGHT(new Vector2D(1, 0));
 
     private final Vector2D vectorDirection;
 
-    VectorDirection (final Vector2D vector2d) {
+    VectorDirection(final Vector2D vector2d) {
         this.vectorDirection = vector2d;
     }
 
