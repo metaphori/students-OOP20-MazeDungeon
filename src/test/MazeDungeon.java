00633f00911f7f427ca828cec6f0b0c6c8388d06
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-03-23 09:27:24 +0100

    removed usless test

--- src/test/MazeDungeon.java
+++ src/test/MazeDungeon.java
@@ -3,11 +3,13 @@ package test;
 import gamestructure.mainmenu.MainMenuController;
 import gamestructure.mainmenu.MainMenuControllerImpl;
 
-public class MazeDungeon {
+public final class MazeDungeon {
+
+    private MazeDungeon() {
+    }
 
     public static void main(final String[] args) {
         final MainMenuController controller = new MainMenuControllerImpl();
         controller.setup();
     }
-
 }
